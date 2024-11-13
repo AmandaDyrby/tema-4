0114017b2715cc07957dd8f97e912ba1c6fc48e9
amanda andersen <aman0002@stud.kea.dk>
2024-11-13 10:47:21 +0100

    poop works

--- script.js
+++ script.js
@@ -16,17 +16,15 @@ function startGame() {
   // skjul andre skærme
   // vis game skærm
   points = 0;
-  document.querySelector("#score_board").innerHTML = points;
+  document.querySelector("#points").innerHTML = points;
   lives = 3;
-  document.querySelector("#life_board").innerHTML = lives;
+  document.querySelector("#lives").innerHTML = lives;
 
   //start timer animation
 
   //poop random position + random delay
   //seed random position + random delay
   //feather random position + random delay
-
-  // start fall animation on elements
 
   document.querySelector("#poop_container1").classList.add("fall");
   document.querySelector("#seed_container1").classList.add("fall");
@@ -41,77 +39,98 @@ function startGame() {
   //feather clickable
 }
 
-//if player clicks on poop
 function clickPoop() {
+  console.log("clickPoop");
   document
     .querySelector("#poop_container1")
     .removeEventListener("mousedown", clickPoop);
 
   points++;
-  document.querySelector("#score_board").innerHTML = points;
+  document.querySelector("#points").innerHTML = points;
   document.querySelector("#poop_container1").classList.add("freeze");
   document.querySelector("#poop_sprite1").classList.add("disappear_good");
   // play sound
+
+  document
+    .querySelector("#poop_sprite1")
+    .addEventListener("animationend", poopClickReset);
 }
 
-// if poopDisappear animation done
 function poopClickReset() {
+  console.log("clickPoopReset");
   document.querySelector("#poop_container1").classList.remove("freeze");
   document.querySelector("#poop_sprite1").classList.remove("disappear_good");
+
+  document.querySelector("#poop_container1").classList.remove("fall");
+  document.querySelector("#poop_container1").offsetLeft;
   document.querySelector("#poop_container1").classList.add("fall");
   document
     .querySelector("#poop_container1")
     .addEventListener("mousedown", clickPoop);
-  //show element again
+
   //new random position
 }
 
 //if poop animation ends
 function poopFallReset() {
   lives--;
-  document.querySelector("#life_board").innerHTML = lives;
+  document.querySelector("#lives").innerHTML = lives;
   //show element again
   //new random position
   document.querySelector("#poop_container1").classList.add("fall");
 }
 
-//if palyer clicks seed
 function clickSeed() {
+  console.log("clickSeed");
   document
     .querySelector("#seed_container1")
     .removeEventListener("mousedown", clickSeed);
+
   points--;
   document.querySelector("#score_board").innerHTML = points;
-  document.querySelector("#seed_container1").classList.add("disappear_bad");
+  document.querySelector("#seed_container1").classList.add("freeze");
+  document.querySelector("#seed_sprite1").classList.add("disappear_bad");
   //play sound
-}
 
-//if seedDisappear animation done
-function seedReset() {
-  //show element again
-  //new random position
-  //restart fall animation
-}
-
-//if player clicks feather
-function clickFeather() {
   document
-    .querySelector("selector")
-    .removeEventListener("mousedown", clickFeather);
-  if (lives < 3) {
-    lives++;
-  }
-  document.querySelector("#life_board").innerHTML = lives;
-  // play sound
-  // start featherDisappear animation
+    .querySelector("#seed_sprite1")
+    .addEventListener("animationend", seedReset);
 }
 
-//if featherDisappear animation done
-function featherClickReset() {
-  //show element again
+function seedReset() {
+  console.log("seedReset");
+  document.querySelector("#seed_container1").classList.remove("freeze");
+  document.querySelector("#seed_sprite1").classList.remove("disappear_bad");
+
+  document.querySelector("#seed_container1").classList.remove("fall");
+  document.querySelector("#seed_container1").offsetLeft;
+  document.querySelector("#seed_container1").classList.add("fall");
+  document
+    .querySelector("#seed_container1")
+    .addEventListener("mousedown", clickPoop);
+
   //new random position
-  //restart fall animation
 }
+
+// //if player clicks feather
+// function clickFeather() {
+//   document
+//     .querySelector("selector")
+//     .removeEventListener("mousedown", clickFeather);
+//   if (lives < 3) {
+//     lives++;
+//   }
+//   document.querySelector("#life_board").innerHTML = lives;
+//   // play sound
+//   // start featherDisappear animation
+// }
+
+// //if featherDisappear animation done
+// function featherClickReset() {
+//   //show element again
+//   //new random position
+//   //restart fall animation
+// }
 
 //if lives=0 // if timer animation done
 function stopGame() {
