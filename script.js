2a2274df3af2cd5b3f686d348a7179a14b6f6105
amanda andersen <aman0002@stud.kea.dk>
2024-11-18 11:05:03 +0100

    aaaand it broke

--- script.js
+++ script.js
@@ -7,11 +7,12 @@ function rng(max) {
 }
 
 const poop1 = document.querySelector("#poop_container1");
+const poop2 = document.querySelector("#poop_container2");
 const seed1 = document.querySelector("#seed_container1");
+const seed2 = document.querySelector("#seed_container2");
 
 function showPage() {
   console.log("show page");
-  // skjul andre skærme
   startGame();
 }
 
@@ -35,52 +36,65 @@ function startGame() {
   poop1.addEventListener("animationiteration", poopClickReset);
   poop1.addEventListener("mousedown", clickPoop);
   poop1.addEventListener("animationiteration", poopFallReset);
-  //poop random delay
+
+  //initialize poop2
+  poop2.classList.add("pos" + rng(9), "fallGood", "delay" + rng(4));
+  poop2.addEventListener("animationiteration", poopClickReset);
+  poop2.addEventListener("mousedown", clickPoop);
+  poop2.addEventListener("animationiteration", poopFallReset);
 
   //initialze seed1
   seed1.classList.add("pos" + rng(9), "fallBad", "delay" + rng(4));
   seed1.addEventListener("animationiteration", seedReset);
   seed1.addEventListener("mousedown", clickSeed);
-  //seed random delay
+
+  //initialze seed2
+  seed2.classList.add("pos" + rng(9), "fallBad", "delay" + rng(4));
+  seed2.addEventListener("animationiteration", seedReset);
+  seed2.addEventListener("mousedown", clickSeed);
 
   //initialize feather1
-  //feather random delay
+  //initialize feather2
+  //initialize feather3
 }
 
 function clickPoop() {
   console.log("clickPoop");
-  document
-    .querySelector("#poop_container1")
-    .removeEventListener("mousedown", clickPoop);
 
+  this.removeEventListener("mousedown", clickPoop);
+
+  //add points on poop click
   points++;
   document.querySelector("#points").innerHTML = points;
-  poop1.classList.add("freeze");
-  document.querySelector("#poop_sprite1").classList.add("disappear_good");
   // play sound
 
-  document
-    .querySelector("#poop_sprite1")
-    .addEventListener("animationend", poopClickReset);
+  //poop disappear animation
+  poop1.classList.add("freeze");
+  this.firstElementChild.classList.add("disappear_good");
+
+  //reset poop after animation end
+  this.firstElementChild.addEventListener("animationend", poopClickReset);
 }
 
 function poopClickReset() {
   //console.log("clickPoopReset");
-  poop1.classList.remove("freeze");
-  document.querySelector("#poop_sprite1").classList.remove("disappear_good");
+  this.classList.remove("freeze");
+  this.firstElementChild.classList.remove("disappear_good");
 
-  poop1.classList = "";
-  poop1.offsetLeft;
-  poop1.classList.add("pos" + rng(9), "fallGood");
-  document
-    .querySelector("#poop_container1")
-    .addEventListener("mousedown", clickPoop);
+  //reset click
+  this.classList = "";
+  this.offsetLeft;
+  this.classList.add("pos" + rng(9), "fallGood");
+  this.firstElementChild.addEventListener("mousedown", clickPoop);
 }
 
 function poopFallReset() {
+  //lose life if poop hits floor
   lives--;
   document.querySelector("#lives").innerHTML = lives;
   //play sound
+
+  //game over if lives reaches 0
   if (lives < 1) {
     stopGame();
   }
@@ -88,30 +102,31 @@ function poopFallReset() {
 
 function clickSeed() {
   console.log("clickSeed");
-  seed1.removeEventListener("mousedown", clickSeed);
+  this.removeEventListener("mousedown", clickSeed);
 
+  //subtract point if seed is clicked
   points--;
   document.querySelector("#points").innerHTML = points;
-  seed1.classList.add("freeze");
-  document.querySelector("#seed_sprite1").classList.add("disappear_bad");
   //play sound
 
-  document
-    .querySelector("#seed_sprite1")
-    .addEventListener("animationend", seedReset);
+  //seed disappear animation
+  this.classList.add("freeze");
+  document.querySelector("#seed_sprite1").classList.add("disappear_bad");
+
+  //reset seed after animation end
+  this.firstElementChild.addEventListener("animationend", seedReset);
 }
 
 function seedReset() {
   //console.log("seedReset");
-  seed1.classList.remove("freeze");
-  document.querySelector("#seed_sprite1").classList.remove("disappear_bad");
+  this.classList.remove("freeze");
+  this.firstElementChild.classList.remove("disappear_bad");
 
-  seed1.classList = "";
-  seed1.offsetLeft;
-  seed1.classList.add("pos" + rng(9), "fallBad");
-  document
-    .querySelector("#seed_container1")
-    .addEventListener("mousedown", clickSeed);
+  //reset seed
+  this.classList = "";
+  this.offsetLeft;
+  this.classList.add("pos" + rng(9), "fallBad");
+  this.addEventListener("mousedown", clickSeed);
 }
 
 // function clickFeather() {
@@ -141,20 +156,24 @@ function stopGame() {
     .querySelector("#time_container")
     .removeEventListener("animationend", stopGame);
 
-  poop1.classList = "";
-  poop1.firstElementChild.classList = "";
+  this.classList = "";
+  this.firstElementChild.classList = "";
 
   poop1.removeEventListener("mousedown", clickPoop);
   poop1.removeEventListener("animationiteration", poopClickReset);
   poop1.removeEventListener("animationend", poopClickReset);
-
-  seed1.classList = "";
-  seed1.firstElementChild.classList = "";
+  poop2.removeEventListener("mousedown", clickPoop);
+  poop2.removeEventListener("animationiteration", poopClickReset);
+  poop2.removeEventListener("animationend", poopClickReset);
 
   seed1.removeEventListener("mousedown", clickSeed);
   seed1.removeEventListener("animationiteration", seedReset);
   seed1.removeEventListener("animationend", seedReset);
+  seed2.removeEventListener("mousedown", clickSeed);
+  seed2.removeEventListener("animationiteration", seedReset);
+  seed2.removeEventListener("animationend", seedReset);
 
+  //which end screen?
   if (lives <= 0) {
     gameOver();
   } else if (points >= 10) {
